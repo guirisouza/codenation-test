bdd410a826bde6ac0dd0226f69efa678b9aa1a8a
Guilherme Ribeiro <guilherme@martizi.com>
2020-04-15 17:37:40 -0300

    decifrando e retornando o resultado

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 - [x] Chamar uma requisição http
 - [x] Pegar a mensagem criptografada do retorno
 - [x] Descriptografar a substituição da letra do alfabeto avançado um determinado número de casas
-- [ ] Criptografar novamente a mensagem usando base sha1
-- [ ] Fazer uma requisição post novamente com a nova mensagem criptograda
+- [x] Criptografar novamente a mensagem usando base sha1
+- [x] Fazer uma requisição post novamente com a nova mensagem criptograda
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const axios = require('axios')
+const sha1 = require('sha1')
+const FormData = require('form-data');
+const fs = require('fs');
+
+// função que recebe uma letra decifra e o numero de casas e retorna ela decifrada
 
 let sirCalc = (word, charNum) => {
     let newIndex = 0
@@ -18,6 +23,8 @@ let sirCalc = (word, charNum) => {
     return result
 }
 
+// função que valida a mensagem recebida e faz os tratamentos iniciais
+
 let sirDecipherer = (message, numChar) => {
     let input = message.split('')
     
@@ -32,17 +39,55 @@ let sirDecipherer = (message, numChar) => {
         }
     });
     newMessage = newMessage.join('')
-    console.log('Rresult: ', newMessage)
+    console.log('Result: ', newMessage)
+    return newMessage
 }
 
-sirDecipherer('vkxlkizout (ot jkyomt) oy ginokbkj tuz cnkt znkxk oy tuznotm suxk zu gjj, haz xgznkx cnkt znkxk oy tuznotm suxk zu zgqk gcge. gtzuotk jk ygotz-kdavkxe', 6)
-// console.log(sirCalc('a'))
+// Requisição que chama a api
+axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=69ebcc665d5918fd8492341920444b20374b7745')
+    .then(response=>{
+        // construção do novo objeto com a mensagem decifrada e o resumo critográfico
+        message = response.data
+        message.decifrado = sirDecipherer(message.cifrado, message.numero_casas)
+        message.resumo_criptografico = sha1(message.decifrado)
 
-// axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=69ebcc665d5918fd8492341920444b20374b7745')
-//     .then(response=>{
-//         message = response.data
-        
-//     })
-//     .catch(err=>{
-//         console.error(err)
-// })
+        // criação do arquivo JSON
+        var jsonContent = JSON.stringify(message);
+        fs.writeFile("answer.json", jsonContent, 'utf8', function (err) {
+            if (err) {
+                console.log("An error occured while writing JSON Object to File.");
+                return console.log(err);
+            }
+         
+            console.log("JSON file has been saved.");
+            
+            // Criação do form e stream do arquivo json
+            let form_data = new FormData();
+            form_data.append('answer',fs.createReadStream('./answer.json'));
+            console.log(form_data)
+            console.log("\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n")
+
+            // criação das configurações do header do post req
+            const request_config = {
+                headers: {
+                  "Content-Type": "multipart/form-data"
+                },
+                data: form_data
+              };
+            
+            // Requisição do post
+            axios.post('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=69ebcc665d5918fd8492341920444b20374b7745', form_data, request_config)
+            .then(response=>{
+                console.log('@@@@@@@@@@@@@@@@@@@@@',response)
+            })
+            .catch(err=>{
+                console.error(err)
+            })
+        });
+        console.log(message)
+
+
+    })
+    .catch(err=>{
+        console.error(err)
+})
